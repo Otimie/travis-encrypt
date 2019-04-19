af4e7fc44fd86c396a56a8fed6e147a2ac8a2427
Lachlan Blake <lachlan.blake10@gmail.com>
2019-04-19 23:22:17 +1000

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,3 +6,11 @@ require 'travis'
 
 repo = Travis::Repository.find('rails/rails')
 puts repo.encrypt('FOO=bar')
+
+payload = {key: 'value'}
+
+uri = URI.parse("https://api.travis-ci.com/")
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Post.new(uri.request_uri, header)
+request.body = user.to_json
+response = http.request(request)
